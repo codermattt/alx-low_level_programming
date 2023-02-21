cd320deb9f9aa189345c790e20a8aea98efd64a9
Mathew <mathewhendricks12@gmail.com>
2023-02-21 03:51:01 -0800

    source code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _putchar - writes the character c to stdout
+ * header for function _putchar above
+ *
+ * Return: 0 (Success)
+ * On error, -1 is returned, errno is set appropriately
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
